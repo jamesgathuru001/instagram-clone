be6c68b7ac29525a9c850cc5500d0fadfbe294f2
James <jamesgathuru001@gmail.com>
2019-06-25 07:33:26 +0300

    adding bootstrap to my installed app

--- instagram/settings.py
+++ instagram/settings.py
@@ -79,8 +79,8 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tribune',
-        'USER': 'instagram',
+        'NAME': 'instagram',
+        'USER': 'james',
     'PASSWORD': 'james55',
     }
 }
@@ -123,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), ]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
